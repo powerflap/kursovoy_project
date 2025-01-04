48061f676d2614e4af070db3bbe3a122150e0699
Женёк <muratov_yevgeniy@vk.com>
2025-01-04 16:00:54 +0300

    nixyaNeponuatno

--- src/Employee.java
+++ src/Employee.java
@@ -1,18 +1,32 @@
 public class Employee {
-   private String fio;
-   private String department;
-   private int salary;
+    private static int length;
+    String name;
+    String surname;
+    String patronumic;
+    String department;
+    int salary;
+   
 
-    public Employee(String fio, String department, int salary) {
-        this.fio = fio;
+    public Employee(String name,String surname,String patronymic, String department, int salary) {
+        this.name = name;
+        this.surname =surname;
+        this.patronumic =patronymic;
         this.department = department;
         this.salary = salary;
 
     }
-    public String getFio() {return this.fio;}
+    public String getName() {return this.name;}
+    public String getSurname() {return this.surname;}
+    public String getPatronumic() {return this.patronumic;}
     public String getDepartment(){return this.department;}
     public int getSalary(){return this.salary;}
-    public void setFio(String fio) {this.fio = fio;}
+    public void setName(String name) {this.name = name;}
+    public void setSurname(String surname) {this.surname = surname;}
+    public void setPatronumic(String patronumic) {this.patronumic = patronumic;}
     public void setDepartment(String department) {this.department = department;}
     public void setSalary(int salary) {this.salary = salary;}
-}
+    public void getEmployeesInfo() {
+
+
+
+}}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
+        Employee[] employee = new Employee[0];
+        employee[0] = new Employee(" Иван ", "Иванов", "Иванович","5",32323);
+        employee[0] = new Employee(" Серега ", "Сергеев", "Сергеевич","4",23123);
+        employee[0] = new Employee(" Владимир ", "Владимиров", "Владимирович","5",23234);
+        employee[0] = new Employee(" Степан ", "Разин", "Тимофеевич","4",44444);
+        employee[0] = new Employee(" Ирина ", "Белкова", "Геннадьевна","3",100000);
+        employee[0] = new Employee(" Светлана ", "Гербер", "Максимовна","3",45666);
+        employee[0] = new Employee(" Берта ", "Лапина", "Евгеньевна","2",62300);
+        employee[0] = new Employee(" Кирилл ", "Узелков", "Алексеевич","2",16500);
+        employee[0] = new Employee(" Виталий ", "Пальцев", "Борисович","1",33200);
+        employee[0] = new Employee(" Мирослава ", "Бублик", "Ашотовна", "1", 33123);
+        System.out.println(employee[0]);
+
 
     }
 }
